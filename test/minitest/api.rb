c9d5f51d6bed0a6b8c1023007b30e912b4366400
Stas Ukolov <ukoloff@gmail.com>
2016-04-23 22:38:06 +0500

    Send message

--- test/minitest/api.rb
+++ test/minitest/api.rb
@@ -1,17 +1,34 @@
 require 'json'
+require 'net/http'
 
 module AppVeyor
   module Worker
 
-    def self.api
-      ENV['APPVEYOR_API_URL'] || skip!
-    end
-
     def self.message msg, details=nil
       x = api or return
+      body = JSON.generate category: 'info',
+        message: msg,
+        details: details
+      x.post '/api/build/messages',
+        body,
+        'Content-Length'=>body.length,
+        'Content-Type'=>'application/json'
     end
 
     private
+
+    def self.api
+      return if false===@http
+      return @http if @http
+      unless z = ENV['APPVEYOR_API_URL']
+        @http = false
+        return
+      end
+      z = URI z
+      @http = x = Net::HTTP.start z.host, z.port
+      x.use_ssl='https'==z.scheme
+      x
+    end
 
     def self.skip!
       unless @skipped
